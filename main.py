3a0ddf2e5b6504573aa7b86597bf59fc638b6902
thertt1 <mixrudya@gmail.com>
2025-04-22 19:11:36 -0500

    Quick commit makes main easier to work with

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import pygame
 from constants import *
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-screen_color = (0, 0, 0)
 
-while True:
-	for event in pygame.event.get():
-    	if event.type == pygame.QUIT:
-        	return
-	screen.fill(screen_color)
-	pygame.display.flip()
+
+
 
 def main():
+	# Initializes pygame
 	pygame.init()
+	
+	#Screen setup
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	screen_color = (0, 0, 0)
+
+	#Clock setup
+	clock = pygame.time.Clock()
+	dt = 0
+
+	#Game loop
+	while True:
+		for event in pygame.event.get():
+			#X button works on window
+			if event.type == pygame.QUIT:
+				return
+
+		#Makes screen black
+		screen.fill(screen_color)
+		pygame.display.flip()
+		# Sets clock
+		dt = clock.tick(60) / 1000
+	
+
 	print("Starting Asteroids!")
 	print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')
 
